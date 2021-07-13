ddf8f571847215d8da2d03f179951e2b21c471cc
Jordan Mathews <jordanmathews04@gmail.com>
2021-07-13 19:46:13 +0200

    My applications table and controller

---
+++ force-app/main/default/lwc/aareg_myApplications/aareg_myApplications.js
@@ -0,0 +1,51 @@
+import { LightningElement, wire, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import Id from '@salesforce/user/Id';
+import getUsersApplications from '@salesforce/apex/AAREG_MyApplicationsController.getUsersApplications';
+
+const COLUMNS = [
+    { label: 'Søknadsnummer', fieldName: 'Name', type: 'text', hideDefaultActions: true },
+    { label: 'Innlevertdato', fieldName: 'ApplicationSubmittedDate__c', type: 'date', hideDefaultActions: true },
+    { label: 'Status', fieldName: 'Status__c', type: 'text', hideDefaultActions: true },
+    {
+        type: 'button',
+        fixedWidth: 150,
+        typeAttributes: {
+            label: 'Se Søknad',
+            title: 'Se Søknad',
+            name: 'Søknad',
+            variant: 'base'
+        }
+    }
+];
+
+export default class Aareg_myApplications extends NavigationMixin(LightningElement) {
+    @track applications;
+    columns = COLUMNS;
+    currentUser = Id;
+    error;
+
+    @wire(getUsersApplications, { userId: '$currentUser' })
+    applications(result) {
+        if (result.data) {
+            if (result.data.length > 0) {
+                this.applications = result.data;
+            }
+            this.error = undefined;
+        } else if (result.error) {
+            console.log(error);
+            this.error = error;
+        }
+    }
+
+    viewApplication(event) {
+        const row = event.detail.row;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: row.Id,
+                actionName: 'view'
+            }
+        });
+    }
+}
