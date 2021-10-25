fa727b00ab4fc510c800e93e6ee2cfaba053df5c
annolse <ann.kristin.olsen@nav.no>
2021-10-25 22:37:55 +0200

    LN 71 added

--- force-app/main/default/lwc/aareg_agreement/aareg_agreement.js
+++ force-app/main/default/lwc/aareg_agreement/aareg_agreement.js
@@ -1,6 +1,6 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
-import { getRecord, getFieldValue,updateRecord } from 'lightning/uiRecordApi';
+import { getRecord, getFieldValue, getRecordNotifyChange } from 'lightning/uiRecordApi';
 import NAME from '@salesforce/schema/Agreement__c.Name';
 import DECISION from '@salesforce/schema/Agreement__c.Decision__c';
 import API_ACCESS from '@salesforce/schema/Agreement__c.APIAccess__c';
@@ -68,7 +68,7 @@ export default class Aareg_agreement extends NavigationMixin(LightningElement) {
       })
         .then((result) => {
           this.readOnly = true;
-          updateRecord({fields: this.recordId});
+          getRecordNotifyChange([{recordId: this.recordId}]);
         })
         .catch((error) => {
           console.error(error);
@@ -79,6 +79,7 @@ export default class Aareg_agreement extends NavigationMixin(LightningElement) {
     } else {
       this.isLoading = false;
     }
+    
   }
 
   handleAgreementCancellation() {
