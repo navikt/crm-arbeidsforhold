155ed7634a935f6e8afec5a78b71c6787aebacca
eirikfladby <eirik.fladby@gmail.com>
2022-10-18 17:26:03 +0200

    add default value to checkpicklist component and use in flow

--- force-app/main/default/lwc/checklistPicklist/checklistPicklist.js
+++ force-app/main/default/lwc/checklistPicklist/checklistPicklist.js
@@ -4,14 +4,27 @@ export default class ChecklistPicklist extends LightningElement {
   @api valueString; // Arrives from the Flow
   @api valueChosen; // Goes to the Flow
   @api label;
-
+  @api defaultValue;
+  arrayValues = [];
   picklistOptionValues = []; // Contains the options for the Picklist
 
   connectedCallback() {
-    var arrayValues = this.valueString.split(';');
-    for (var i = 0; i < arrayValues.length; i++) {
-      this.picklistOptionValues.push(arrayValues[i]);
+    this.arrayValues = this.valueString.split(';');
+    for (var i = 0; i < this.arrayValues.length; i++) {
+      this.picklistOptionValues.push(this.arrayValues[i]);
     }
+  }
+
+  renderedCallback() {
+    this.setDefaultValue();
+  }
+
+  setDefaultValue() {
+    this.arrayValues.forEach(element => {
+      if (this.defaultValue === element) {
+        this.valueChosen = this.template.querySelector('select').value = element;
+      }
+    });
   }
 
   assignValue(event) {
