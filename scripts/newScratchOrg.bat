98f084c257d795b279106a5cf75b6e7f0c544ee9
annolse <ann.kristin.olsen@nav.no>
2025-12-15 11:31:08 +0100

    remove LWC

--- scripts/newScratchOrg.bat
+++ scripts/newScratchOrg.bat
@@ -6,8 +6,8 @@ set ORG_ALIAS=crm-arbeidsforhold
 set ORG_DURATION=30
 set SECRET=%1
 
-echo Cleaning previous scratch org...
-call sf org delete scratch --target-org %ORG_ALIAS% --no-prompt 2>NUL
+//echo Cleaning previous scratch org...
+//call sf org delete scratch --target-org %ORG_ALIAS% --no-prompt 2>NUL
 
 echo "Oppretter scratch org"
 call sf org create scratch --alias %ORG_ALIAS% --set-default --definition-file config/project-scratch-def.json --duration-days %ORG_DURATION% --wait 10
@@ -67,8 +67,8 @@ echo "Installerer crm-community-base 135."
 call sf force:package:install --package 04tQC0000012aDJYAY -r --installation-key %1 --wait 4 --publish-wait 4
 
 echo "INSTALLERER"
-echo "Installerer crm-platform-email-scheduling 1.6.0."
-call sf force:package:install --package 04t2o000000yRSSAA2 -r --installation-key %1 --wait 4 --publish-wait 4
+echo "Installerer crm-platform-email-scheduling 1.8.0."
+call sf force:package:install --package 04tKB000000Y5TZYA0 -r --installation-key %1 --wait 4 --publish-wait 4
 
 echo "INSTALLERER"
 echo "Installerer crm-journal-utilities 0.53.0.."
@@ -86,8 +86,12 @@ echo "INSTALLERER"
 echo "Installerer crm-henvendelse 0.192.0"
 call sf force:package:install --package 04tQC0000012crpYAA -r --installation-key %1 --wait 5 --publish-wait 4
 
-echo "Dytter kildekoden til scratch org'en"
-call sf project deploy start --target-org %ORG_ALIAS% --wait 10
+//echo "Dytter kildekoden til scratch org'en"
+//call sf project deploy start --target-org %ORG_ALIAS% --wait 10
+
+echo "INSTALLERER"
+echo "Installerer crm-arbeidsforhold 0.297.0 alpha18"
+call sf force:package:install --package 04tQC0000012jOfYAI -r --installation-key %1 --wait 5 --publish-wait 4
 
 echo "Tildeler tilatelsessett til brukeren"
 call sf org assign permset -n AAREG_Arbeidsforhold_Saksbehandling
