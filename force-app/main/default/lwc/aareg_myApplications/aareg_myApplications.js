1e5db1d9b7683f3174e2d505bf08abbbf83197ab
annolse <ann.kristin.olsen@nav.no>
2026-02-16 13:33:28 +0100

    to test

--- force-app/main/default/lwc/aareg_myApplications/aareg_myApplications.js
+++ force-app/main/default/lwc/aareg_myApplications/aareg_myApplications.js
@@ -112,8 +112,14 @@ export default class Aareg_myApplications extends NavigationMixin(LightningEleme
           console.log('Retrieved URL:', url);
           if (url) {
             // Prepend the domain to the URL
-            const fullUrl = window.location.origin + url;
-            console.log('Full URL:', fullUrl);
+            const siteOrigin = window.location.origin;
+            if (siteOrigin === 'https://navdialog--sit2.sandbox.my.site.com') {
+              this.siteURL = siteOrigin + '/aaregisteret' + url;
+            } else { // Prod
+              this.siteURL = siteOrigin + url;
+            }
+            //const fullUrl = window.location.origin + url;
+            //console.log('Full URL:', fullUrl);
 
             // Use NavigationMixin to navigate to the URL
             this[NavigationMixin.Navigate]({
