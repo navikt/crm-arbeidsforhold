251f2e6e7fb1488cf3f92f14ea698ced315a346a
annolse <ann.kristin.olsen@nav.no>
2026-08-13 12:20:12 +0200

    Fix typos in application contact message and update decision date field in data table

--- force-app/externalApplication/lwc/aareg_myApplications/aareg_myApplications.js
+++ force-app/externalApplication/lwc/aareg_myApplications/aareg_myApplications.js
@@ -10,7 +10,7 @@ const COLUMNS = [
   { label: 'Søknadsnummer', fieldName: 'Name', type: 'text', hideDefaultActions: true },
   { label: 'Dato innlevert', fieldName: 'ApplicationSubmittedDate__c', type: 'date', hideDefaultActions: true },
   { label: 'Forventet svar', fieldName: 'ApplicationDeadlineForReply__c', type: 'date', hideDefaultActions: true},
-  { label: 'Vedtaksdato', fieldName: 'DecisionDate__c ', type: 'date', hideDefaultActions: true },
+  { label: 'Vedtaksdato', fieldName: 'DecisionDate__c', type: 'date', hideDefaultActions: true },
   { label: 'Status', fieldName: 'Status__c', type: 'text', hideDefaultActions: true },
   {
     type: 'button',
