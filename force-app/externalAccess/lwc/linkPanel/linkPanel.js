40aa803fd3765112f25e2e456427bd59bf26b605
Kenneth Sørensen <kenneth.sorensen@nav.no>
2026-09-13 13:26:47 +0200

    Fix: Add missing newlines at the end of several Apex class and metadata files

--- force-app/externalAccess/lwc/linkPanel/linkPanel.js
+++ force-app/externalAccess/lwc/linkPanel/linkPanel.js
@@ -3,30 +3,30 @@ import { NavigationMixin } from 'lightning/navigation';
 import communityBase from '@salesforce/community/basePath';
 
 export default class LinkPanel extends NavigationMixin(LightningElement) {
-  @api title;
-  @api subTitle;
-  @api relativePath;
-  @api unreadCount = 0;
-  basePath = communityBase;
+    @api title;
+    @api subTitle;
+    @api relativePath;
+    @api unreadCount = 0;
+    basePath = communityBase;
 
-  get hasUnread() {
-    return (this.unreadCount || 0) > 0;
-  }
+    get hasUnread() {
+        return (this.unreadCount || 0) > 0;
+    }
 
-  get panelTitle() {
-    return this.title;
-  }
+    get panelTitle() {
+        return this.title;
+    }
 
-  get panelSubTitle() {
-    return this.subTitle;
-  }
+    get panelSubTitle() {
+        return this.subTitle;
+    }
 
-  navigateToPage(event) {
-    this[NavigationMixin.Navigate]({
-      type: 'comm__namedPage',
-      attributes: {
-        name: this.relativePath
-      }
-    });
-  }
+    navigateToPage(event) {
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: this.relativePath
+            }
+        });
+    }
 }
