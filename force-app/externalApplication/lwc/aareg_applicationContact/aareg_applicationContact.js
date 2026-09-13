2d072f48ffc7b58cb4edc8198eee06a8969e9ada
Kenneth Sørensen <kenneth.sorensen@nav.no>
2026-09-13 13:33:44 +0200

    Refactor: Ensure consistent formatting and add missing newlines in Apex classes and LWC files
    - Added missing newlines at the end of multiple Apex classes and their corresponding metadata files.
    - Updated apiVersion in AAREG_MyApplicationsControllerTest.cls-meta.xml and AAREG_distributionAccessRequestTest.cls-meta.xml to 66.0 for consistency.
    - Cleaned up XML declaration in several metadata files to remove any invisible characters.

--- force-app/externalApplication/lwc/aareg_applicationContact/aareg_applicationContact.js
+++ force-app/externalApplication/lwc/aareg_applicationContact/aareg_applicationContact.js
@@ -1,119 +1,119 @@
 import { LightningElement, api } from 'lwc';
 import { validateEmail } from 'c/aareg_helperClass';
 export default class aareg_applicationContact extends LightningElement {
-  @api record;
-  @api readOnly;
-  contact;
+    @api record;
+    @api readOnly;
+    contact;
 
-  renderedCallback() {
-    this.name = this.template.querySelector('[data-id="Name"]');
-    this.phone = this.template.querySelector('[data-id="Phone__c"]');
-    this.email = this.template.querySelector('[data-id="Email__c"]');
-    this.agreementNotification = this.template.querySelector('[data-id="AgreementNotifications__c"]');
-  }
-
-  connectedCallback() {
-    this.initContact();
-  }
-
-  initContact() {
-    this.contact = {
-      uuid: this.record.uuid,
-      Id: this.record.Id ? this.record.Id : null,
-      Name: this.record.Name ? this.record.Name : null,
-      Phone__c: this.record.Phone__c ? this.record.Phone__c : null,
-      Email__c: this.record.Email__c ? this.record.Email__c : null,
-      AgreementNotifications__c: this.record.AgreementNotifications__c,
-      ChangeNotifications__c: this.record.ChangeNotifications__c,
-      ErrorMessageNotifications__c: this.record.ErrorMessageNotifications__c,
-      SecurityNotifications__c: this.record.SecurityNotifications__c
-    };
-    this.publishChange();
-  }
-
-  /*************** Change handlers ***************/
-
-  handleInputChange(event) {
-    this.contact[event.target.dataset.id] = event.target.value;
-    this.publishChange();
-  }
-
-  handleCheckboxChange(event) {
-    this.contact[event.target.dataset.id] = event.target.checked;
-    this.publishChange();
-  }
-
-  publishChange() {
-    const changeEvent = new CustomEvent('contactchange', { detail: this.contact });
-    this.dispatchEvent(changeEvent);
-  }
-
-  publishError() {
-    const changeEvent = new CustomEvent('validationerror', { detail: true });
-    this.dispatchEvent(changeEvent);
-  }
-
-  /*************** Validation ***************/
-  @api
-  validate() {
-    this.resetErrors();
-    let error = false;
-    if (this.checkNulls(this.contact.Name)) {
-      this.setErrorFor(this.name, 'Obligatorisk');
-      error = true;
+    renderedCallback() {
+        this.name = this.template.querySelector('[data-id="Name"]');
+        this.phone = this.template.querySelector('[data-id="Phone__c"]');
+        this.email = this.template.querySelector('[data-id="Email__c"]');
+        this.agreementNotification = this.template.querySelector('[data-id="AgreementNotifications__c"]');
     }
-    if (validateEmail(this.contact.Email__c)) {
-      this.setErrorFor(this.email, 'E-post må være gyldig format.');
-      error = true;
+
+    connectedCallback() {
+        this.initContact();
     }
-    if (this.checkFieldLength(this.contact.Phone__c)) {
-      this.setErrorFor(this.phone, 'Nummer må være 8 siffer.');
-      error = true;
+
+    initContact() {
+        this.contact = {
+            uuid: this.record.uuid,
+            Id: this.record.Id ? this.record.Id : null,
+            Name: this.record.Name ? this.record.Name : null,
+            Phone__c: this.record.Phone__c ? this.record.Phone__c : null,
+            Email__c: this.record.Email__c ? this.record.Email__c : null,
+            AgreementNotifications__c: this.record.AgreementNotifications__c,
+            ChangeNotifications__c: this.record.ChangeNotifications__c,
+            ErrorMessageNotifications__c: this.record.ErrorMessageNotifications__c,
+            SecurityNotifications__c: this.record.SecurityNotifications__c
+        };
+        this.publishChange();
     }
-    return error;
-  }
 
-  @api focusInput() {
-    let invalidFields = this.template.querySelector(':invalid');
+    /*************** Change handlers ***************/
 
-    if (invalidFields) {
-      invalidFields.focus();
-      return true;
+    handleInputChange(event) {
+        this.contact[event.target.dataset.id] = event.target.value;
+        this.publishChange();
     }
-    return false;
-  }
 
-  @api focusAgreementNotification() {
-    this.agreementNotification.focus();
-  }
-
-  checkFieldLength(field) {
-    if (field === null || field.length !== 8) {
-      return true;
-    } 
-    return false;
-  }
-
-  checkNulls(field) {
-    if (field === null || field === '') {
-      return true;
-    } else {
-      return false;
+    handleCheckboxChange(event) {
+        this.contact[event.target.dataset.id] = event.target.checked;
+        this.publishChange();
     }
-  }
 
-  setErrorFor(inputField, message) {
-    this.publishError();
-    let formControl = inputField.parentElement;
-    let small = formControl.querySelector('small');
-    small.innerText = message;
-    formControl.className = 'form-control error';
-  }
+    publishChange() {
+        const changeEvent = new CustomEvent('contactchange', { detail: this.contact });
+        this.dispatchEvent(changeEvent);
+    }
 
-  resetErrors() {
-    let formControl = this.template.querySelectorAll('.form-control');
-    formControl.forEach((element) => {
-      element.classList.remove('error');
-    });
-  }
+    publishError() {
+        const changeEvent = new CustomEvent('validationerror', { detail: true });
+        this.dispatchEvent(changeEvent);
+    }
+
+    /*************** Validation ***************/
+    @api
+    validate() {
+        this.resetErrors();
+        let error = false;
+        if (this.checkNulls(this.contact.Name)) {
+            this.setErrorFor(this.name, 'Obligatorisk');
+            error = true;
+        }
+        if (validateEmail(this.contact.Email__c)) {
+            this.setErrorFor(this.email, 'E-post må være gyldig format.');
+            error = true;
+        }
+        if (this.checkFieldLength(this.contact.Phone__c)) {
+            this.setErrorFor(this.phone, 'Nummer må være 8 siffer.');
+            error = true;
+        }
+        return error;
+    }
+
+    @api focusInput() {
+        let invalidFields = this.template.querySelector(':invalid');
+
+        if (invalidFields) {
+            invalidFields.focus();
+            return true;
+        }
+        return false;
+    }
+
+    @api focusAgreementNotification() {
+        this.agreementNotification.focus();
+    }
+
+    checkFieldLength(field) {
+        if (field === null || field.length !== 8) {
+            return true;
+        }
+        return false;
+    }
+
+    checkNulls(field) {
+        if (field === null || field === '') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    setErrorFor(inputField, message) {
+        this.publishError();
+        let formControl = inputField.parentElement;
+        let small = formControl.querySelector('small');
+        small.innerText = message;
+        formControl.className = 'form-control error';
+    }
+
+    resetErrors() {
+        let formControl = this.template.querySelectorAll('.form-control');
+        formControl.forEach((element) => {
+            element.classList.remove('error');
+        });
+    }
 }
